7ed7b03e9b075d6d0361f3df17301de5e16ab92a
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-27 09:23:59 -0500

    working

--- reducer.js
+++ reducer.js
@@ -1,27 +1,46 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function createStore(reducer, render) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render;
   }
 
-function dispatch(action){
-  state = changeState(state, action)
+  function getState() {
+    return state;
+  };
+
+  dispatch({ type: '@@INIT' });
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
+function changeCount(state = { count: 0 }, action) {
+  debugger;
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+
+    default:
+      return state;
+  }
+}
+
+
+
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+
+let store = createStore(changeCount, render);
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function() {
+  store.dispatch({ type: 'INCREASE_COUNT' });
   render()
-}
-
-function render(){
-  let container = document.getElementById('container')
-  container.textContent = state.count
-}
-
-  dispatch({type: '@@INIT'})
-  
-  let button = document.getElementById('button');
-
-  button.addEventListener('click', function(){
-    dispatch({type: 'INCREASE_COUNT'})
-  })
+});
